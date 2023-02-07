a8a73af0bba5465004983419e107fa1c439ed6f0
Ivan <42667714+ivanchan0808@users.noreply.github.com>
2023-02-07 10:35:55 +0800

    Export the ACL under the dedicated directory.
    Limitiation : 
    The file path must less than 254 characters.
    The ACL export to CVS file format.
    Each directory / sub directory would create a CVS file which list the ACL of that level of folder.

---
+++ export-filesACL.ps1
@@ -0,0 +1,37 @@
+﻿$Path = "D:\"
+$exportCSV="get-acl.csv"
+
+$Folders = Get-ChildItem $Path -Directory -Recurse
+Foreach ($Folder in $Folders) {
+    $FolderPath = $Folder.FullName
+    $Arr = @()
+    $Files = Get-ChildItem $FolderPath -File
+    Foreach ($File in $Files) {
+        $Acl = Get-Acl $File.FullName
+        Foreach ($Access in $Acl.Access) {
+            $Properties = [ordered]@{
+                Path          = $File.FullName
+                IdentityReference = $Access.IdentityReference
+                AccessControlType = $Access.AccessControlType
+                FileSystemRights  = $Access.FileSystemRights
+                IsInherited       = $Access.IsInherited
+            }
+            $Obj = New-Object -TypeName PSObject -Property $Properties
+            $Arr += $Obj
+        }
+    }
+    $Acl = Get-Acl $FolderPath
+    Foreach ($Access in $Acl.Access) {
+        $Properties = [ordered]@{
+            Path          = $FolderPath
+            IdentityReference = $Access.IdentityReference
+            AccessControlType = $Access.AccessControlType
+            FileSystemRights  = $Access.FileSystemRights
+            IsInherited       = $Access.IsInherited
+        }
+        $Obj = New-Object -TypeName PSObject -Property $Properties
+        $Arr += $Obj
+    }
+    $Arr | Select-Object Path,IdentityReference,AccessControlType,FileSystemRights,IsInherited |
+           Export-Csv -Path "$FolderPath\$exportCSV" -NoTypeInformation -Append:$False
+}
